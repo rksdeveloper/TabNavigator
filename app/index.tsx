4ce1f37c588a1498d5a5d1b28844c1bd9f26bd60
rksdeveloper <raj7234271@gmail.com>
2026-03-22 01:06:41 +0530

    update index file

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.content1}>Hello Tony stark</Text>
+      <Link href={"/about"}>Visit this screen</Link>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: 'row',
+    gap: 20
+  },
+  content: {
+    fontSize: 20
+  },
+  content1: {
+    fontSize: 20,
+    color: 'red'
+  },
+})
